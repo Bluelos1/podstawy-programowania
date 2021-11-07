f0d24aeb13a48b8446c2402179f8cc5e0599b1c1
Bluelos1 <s25946@pjwstk.edu.pl>
2021-11-07 13:35:42 +0100

    zajecia

---
+++ src/99beers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+auto main() -> int 
+{
+int a = 99;
+for(;a>0;)
+{
+std::cout << a << " bottles of beer on the wall, " << a <<" bottles of beer.\n";
+std::cout <<" Take one down, pass it around, " << --a << " bottles of beer on the wall... \n";
+}
+if (a==0){
+std::cout << "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall...\n";
+
+}
+
+return 0;
+
+}
